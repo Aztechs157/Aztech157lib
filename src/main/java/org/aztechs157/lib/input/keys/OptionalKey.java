d937c175e0d7ed34dbda5e88c81821bc82797e05
JamesBeeProg <jamesbeeprog@gmail.com>
2022-04-26 20:53:51 -0400

    Fix bug that OptionalKey didn't actually make it optional

--- src/main/java/org/aztechs157/lib/input/keys/OptionalKey.java
+++ src/main/java/org/aztechs157/lib/input/keys/OptionalKey.java
@@ -3,6 +3,6 @@ package org.aztechs157.lib.input.keys;
 public interface OptionalKey extends KeyBase {
     @Override
     default boolean optional() {
-        return false;
+        return true;
     }
 }
