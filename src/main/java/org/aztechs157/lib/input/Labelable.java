31f66f52aa2888ed7b5dd374ab4cf989878d05d8
JamesBeeProg <jamesbeeprog@gmail.com>
2022-07-20 17:07:26 -0400

    a

--- src/main/java/org/aztechs157/lib/input/Labelable.java
+++ src/main/java/org/aztechs157/lib/input/Labelable.java
@@ -3,16 +3,16 @@ package org.aztechs157.lib.input;
 public interface Labelable<T> {
     public T label(final String label);
 
-    static <T> void experimental_autoLabel(final T object) {
-        try {
-            for (final var field : object.getClass().getFields()) {
+    static <T extends Labelable<T>> void experimental_autoLabel(final T object) {
+        for (final var field : object.getClass().getFields()) {
+            try {
                 final var value = field.get(object);
                 if (value instanceof Labelable) {
                     ((Labelable<?>) value).label(field.getName());
                 }
+            } catch (final Exception e) {
+                System.err.println(e);
             }
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
         }
     }
 }
